2c7369640b8b62ba326296a3c6f88d50eb7e840d
Boris Okunskiy <boris@okunskiy.name>
2022-05-31 15:56:27 +0300

    chore: add bundle

---
+++ src/main/index.ts
@@ -0,0 +1,15 @@
+import * as model from './model';
+import * as nodes from './nodes';
+import * as runtime from './runtime';
+import * as schema from './schema';
+import * as types from './types';
+import * as util from './util';
+
+export {
+    model,
+    nodes,
+    runtime,
+    schema,
+    types,
+    util,
+};
