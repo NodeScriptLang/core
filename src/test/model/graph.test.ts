4da378b17e81c0a6f77030fc877ae02fbf1874b0
Boris Okunskiy <boris@okunskiy.name>
2022-06-08 15:04:59 +0300

    chore: add pending tests for Graph model

--- src/test/model/graph.test.ts
+++ src/test/model/graph.test.ts
@@ -1,3 +1,28 @@
 describe('Graph', () => {
 
+    describe('createNode', () => {
+
+        it('adds the node');
+        it('adds a URI to refs');
+        it('does not add the same URI twice');
+
+    });
+
+
+    describe('deleteNode', () => {
+
+        it('removes the node');
+        it('removes unused refs');
+        it('does not remove used refs');
+
+    });
+
+    describe('invariants', () => {
+
+        it('removes extra properties not supported by node definition');
+        it('ensures the correct order properties');
+        it('initializes all properties with default values');
+
+    });
+
 });
