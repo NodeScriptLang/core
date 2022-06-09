1241f7a60feacdf71a8f7e210e86d0bf290c895f
whatkimkong <whatkimkong@gmail.com>
2022-06-09 17:27:11 +0200

    chore: add createNode tests

--- src/test/model/graph.test.ts
+++ src/test/model/graph.test.ts
@@ -33,15 +33,51 @@ describe('Graph', () => {
             assert.strictEqual(graph.refs[node.ref], uri);
         });
 
-        it('does not add the same URI twice'); // create node with a uri, call func with same uri, check that the uri is not the same? or that there are not two of them? how? - look at node structure
+        it('does not add the same URI twice', async () => {
+            const loader = new GraphLoader();
+            const graph = await loader.loadGraph();
+            assert.strictEqual(Object.keys(graph.refs).length, 0);
+            const uri = runtime.defs['math.add'];
+            const node1 = await graph.createNode({
+                uri,
+                node: {},
+            });
+            const node2 = await graph.createNode({
+                uri,
+                node: {},
+            });
+            assert.strictEqual(Object.keys(graph.refs).length, 1);
+            assert.strictEqual(graph.refs[node1.ref], uri);
+            assert.strictEqual(graph.refs[node1.ref], graph.refs[node2.ref]);
+        });
+
+        it('different URIs will save to different refs', async () => {
+            const loader = new GraphLoader();
+            const graph = await loader.loadGraph();
+            assert.strictEqual(Object.keys(graph.refs).length, 0);
+            const uri1 = runtime.defs['math.add'];
+            const uri2 = runtime.defs['any'];
+            const node1 = await graph.createNode({
+                uri: uri1,
+                node: {},
+            });
+            const node2 = await graph.createNode({
+                uri: uri2,
+                node: {},
+            });
+            assert.strictEqual(Object.keys(graph.refs).length, 2);
+            assert.notEqual(graph.refs[node1.ref], graph.refs[node2.ref]);
+        });
+
+        // it('does not create a node with the wrong type of URI');
     });
 
 
     describe('deleteNode', () => {
 
-        it('removes the node'); // create a node, test the delete on this new node, then check if the node is still existing
-        it('removes unused refs'); // create a ref that is unused, call the delete on this ref, check the ref is not there.
-        it('does not remove used refs'); // create a ref that is used, call the delete on this ref, check the ref is still there.
+        it('removes the node'); // create, a graph, create a node, test the delete func on this new node, then check if the node is still existing .length 0 (create graph with node in it)
+        it('removes unused refs'); // **not yet implemented** create a ref that is unused, call the delete on this ref, check the ref is not there.
+        it('does not remove used refs'); // **not yet implemented**create a ref that is used, call the delete on this ref, check the ref is still there.
 
     });
 
