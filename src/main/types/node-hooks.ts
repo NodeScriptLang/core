a3a99fcfc4120c5902c7a692bbb7325eb832466d
Boris Okunskiy <boris@okunskiy.name>
2022-07-18 12:14:47 +0300

    chore: support async update and unmount hooks

--- src/main/types/node-hooks.ts
+++ src/main/types/node-hooks.ts
@@ -2,6 +2,6 @@ import { NodeResult } from './node-result.js';
 
 export interface NodeHooks {
     mount?(state: Record<string, any>, el: HTMLElement): void | Promise<void>;
-    unmount?(state: Record<string, any>): void;
-    update?(state: Record<string, any>, result: NodeResult): void;
+    unmount?(state: Record<string, any>): void | Promise<void>;
+    update?(state: Record<string, any>, result: NodeResult): void | Promise<void>;
 }
