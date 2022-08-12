99ac1e5dba6b7ff1374053eb1538f5159b80ba63
Boris Okunskiy <boris@okunskiy.name>
2022-08-12 11:52:48 +0300

    feat: add ctx.$convertType

--- src/main/types/ctx.ts
+++ src/main/types/ctx.ts
@@ -1,4 +1,7 @@
+import { DataSchema } from './data.js';
+
 export interface GraphEvalContext {
     $cache: Map<string, any>;
     getLocal(key: string): unknown;
+    $convertType<T>(value: unknown, schema: DataSchema<T>): T;
 }
