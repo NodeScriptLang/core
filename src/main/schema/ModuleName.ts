4782f4e43c6382555e43ea5bad5d7ac113aa4694
Boris Okunskiy <boris@okunskiy.name>
2023-06-20 19:21:17 +0300

    chore: drop comment about validating module name

--- src/main/schema/ModuleName.ts
+++ src/main/schema/ModuleName.ts
@@ -2,7 +2,6 @@ import { Schema } from 'airtight';
 
 export type ModuleName = string;
 
-// TODO add validation
 export const ModuleNameSchema = new Schema<ModuleName>({
     id: 'ModuleName',
     type: 'string',
