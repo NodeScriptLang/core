1c21d8b67957c72c8c4e3e3a67a48662c64d5120
Christelle Van Sebroeck <christelle.marie@gmail.com>
2023-10-26 14:04:49 +0100

    chore: add KeyValue and KeyValueSchema back to core (#29)
    * chore: add KeyValue and KeyValueSchema back to core
    
    * fix: add missing index export

---
+++ src/main/schema/KeyValue.ts
@@ -0,0 +1,15 @@
+import { Schema } from 'airtight';
+
+export interface KeyValue {
+    key: string;
+    value: any;
+}
+
+export const KeyValueSchema = new Schema<KeyValue>({
+    id: 'KeyValue',
+    type: 'object',
+    properties: {
+        key: { type: 'string' },
+        value: { type: 'any' },
+    }
+});
